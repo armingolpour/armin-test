9b408dfa1cf2f37853d0a6d69bbf64775bd75101
Armin <armingolpour2@gmail>
2018-07-28 16:18:40 +0430

    Initial Commit

---
+++ sirus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sirus
+//
+//  Created by Amaj on ۱۳۹۷/۵/۶.
+//  Copyright © ۱۳۹۷ Hinext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
